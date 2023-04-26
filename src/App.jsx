ee5bd4bd3a164e3e13df3c19633b7d69d71669ea
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-04-25 22:04:30 -0600

    tercer_commit_primera_rama

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import PetCard from "./components/PetCard"
+
 
 function App() {
+ // parte logica 
+ let nombre = "Rafa" 
 
-  return (
+  return ( 
+    // parte visual
     <>
+      <h1>{nombre}</h1>
+      <h2>{"error " + (400 + 4) + ": page not found"}</h2>
       
+    <PetCard>
+      {/* fragmento */}
+
+
+    </PetCard>
     </>
+
+
   )
 }
 
---
+++ src/components/PetCard.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+const PetCard = () => {
+    return (
+        <div>
+            <h1>Canelita</h1>
+            <h2>Michi</h2>
+            <h2>Vacunas: Si</h2>
+            <h2>Edad: 2 siglos</h2>
+        </div>
+    )
+}
+
+export default PetCard
